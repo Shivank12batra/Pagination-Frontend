554a2f704c700f64285b8965618e19269f84566f
Shivank12batra <Shivank56batra@gmail.com>
2023-05-12 01:30:11 +0530

    backend logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import './App.css'
 function App() {
   const [products, setProducts] = useState([])
   const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(0)
 
   const selectPage = (selectedPage) => {
-    if (selectedPage >= 1 && selectedPage <= products.length/10 && selectedPage !== page) {
+    if (selectedPage >= 1 && selectedPage <= totalPages && selectedPage !== page) {
       setPage(selectedPage)
     }
   }
 
   const fetchProducts = async() => {
-    const response = await fetch('https://dummyjson.com/products?limit=100')
+    const response = await fetch(`https://dummyjson.com/products?limit=100&skip=${page*10 -10}`)
     const data = await response.json()
     setProducts(data.products)
+    setTotalPages(data.total/10)
   }
 
   useEffect(() => {
@@ -24,11 +26,10 @@ function App() {
   return (
     <>
     <h1>Product Pagination</h1>
-    {console.log(products)}
     {products.length > 0 ? (
       <>
       <ul className='products'>
-      {products.slice((products.length/10)*(page-1), page *10).map(({id, title, images}) => {
+      {products.slice((totalPages*(page-1)), page *10).map(({id, title, images}) => {
         return (
           <li key={id} className='product_single'>
             <img src={images[0]} alt={title}/>
@@ -43,12 +44,12 @@ function App() {
     {products.length > 0 ? (
       <div className='pagination'>
       <span onClick={() => setPage((prev) => prev-1)} className={page === 1 ? 'disable_button' : ""}>◀</span>
-      {[...Array(products.length/10)].map((_, i) => {
+      {[...Array(totalPages)].map((_, i) => {
         return (
           <span key={i} className={page === i + 1 ? 'selected_page' : ""} onClick={() => selectPage(i+1)}>{i+1}</span>
         )
       })}
-      <span onClick={() => setPage((next) => next+1)} className={page === products.length/10 ? 'disable_button' : ""}>▶</span>
+      <span onClick={() => setPage((next) => next+1)} className={page === totalPages ? 'disable_button' : ""}>▶</span>
       </div>
     ) : null}
     </>
